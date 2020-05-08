839e657408da061e3bb2a8b061a6213b824efdf2
eddydiep <edouard.diep@gmail.com>
2020-05-08 23:57:43 +0200

    V2.2 - Renamed project into arbre-famille-diep

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to arbre-genealogique!');
+    expect(page.getTitleText()).toEqual('Welcome to arbre-famille-diep!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/arbre-genealogique'),
+      dir: require('path').join(__dirname, './coverage/arbre-famille-diep'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
